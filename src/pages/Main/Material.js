d840a26aef75d56ed4a782b1cd99747cc89358f0
SunYoung <psy010610@naver.com>
2024-02-12 23:25:59 +0900

    Feat: 메인페이지 UI만 제작함

---
+++ src/pages/Main/Material.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "../../styles/main.module.css";
+
+function Material() {
+  return (
+    <div className={styles.material}>
+      <div className={styles.material_img_box}>
+        <img src="https://image.zdnet.co.kr/2019/01/15/sini_1zwN8DfI5uThk94.jpg" />
+      </div>
+      <p>계란</p>
+      <div className={styles.material_day}>D-1</div>
+    </div>
+  );
+}
+
+export default Material;
