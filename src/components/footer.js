4db15d870ac9d143ebf8d1e368944aff4c853fe6
zzihoos <crient0204@naver.com>
2024-02-24 23:14:37 +0900

    feat:mypage, basket 기본 ui 작업완료

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,2 +1,11 @@
 /** 메인 사진 출처 **/
 // <a href="https://kr.freepik.com/free-photo/top-view-on-table-full-of-food_21088404.htm#query=%EC%9D%8C%EC%8B%9D%20%EC%82%AC%EC%A7%84&position=7&from_view=keyword&track=ais&uuid=f58a040b-a514-4b2c-ae9a-5b7005d638cf">Freepik</a>
+import React from 'react'
+
+const footer = () => {
+  return (
+    <div>footer</div>
+  )
+}
+
+export default footer
--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import "../styles/mypage.css";
+import {useNavigate } from "react-router";
 
 const Mypage = () => {
+  const navigate = useNavigate();
   return (
-    <div>Mypage</div>
-  )
-}
+    <div>
+      <header className="mypage-header">Mypage</header>
+      <div className="mypage-top">
+        <div className="bookmark">
+          <span className="bookmark-text">레시피북마크</span>
+          <img className="bookmark-image" src="./images/bookmark.png"></img>
+        </div>
+        <div className="hate">
+          <div className="hate-top">
+            <span className="hate-text">나의 기피 음식</span>
+            <button className="hate-button"><img className="hate-button-img" src="/images/pencil.png"></img></button>
+          </div>
+          <span className="hate-food">기피하는 음식들</span>
+        </div>
+      </div>
+      <div className="memberedit">
+      <span
+        onClick={() => {
+          navigate("/memberedit");
+        }}
+      >
+        회원정보수정
+      </span>
+      
+      <span className="logout">
+        로그아웃
+      </span>
+      </div>
+    </div>
+  );
+};
 
-export default Mypage
+export default Mypage;
