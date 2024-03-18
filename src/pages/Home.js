429ac8ad00dc874b36432557910fc2511a9f7454
SunYoung <psy010610@naver.com>
2024-03-18 18:37:04 +0900

    Feat: 회원가입 UI 구현

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "../App.css";
 import styles from "../styles/home.module.css";
 import { Link } from "react-router-dom";
 import Button from "../components/Button";
@@ -11,7 +10,7 @@ const Home = () => {
         <img src="../../images/logo.jpg"></img>
       </div>
       <p className={styles.home_first}>처음 오셨나요?</p>
-      <Link to="/signup">
+      <Link to="/join">
         <Button name="네, 처음이에요 !"></Button>
       </Link>
       <Link to="/login">
