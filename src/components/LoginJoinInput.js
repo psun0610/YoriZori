755caa17e8cc7ab2395b893607564fd694fdbb11
SunYoung <97274144+psun0610@users.noreply.github.com>
2024-03-18 18:37:04 +0900

    Feat: 회원가입 UI 구현

--- src/components/LoginJoinInput.js
+++ src/components/LoginJoinInput.js
@@ -6,7 +6,11 @@ import PropTypes from "prop-types";
 function LoginSignupInput(props) {
   return (
     <div className={styles.input_container}>
-      <input className={styles.input} onChange={props.onChange}></input>
+      <input
+        type={props.type}
+        className={styles.input}
+        onChange={props.onChange}
+      ></input>
       <p className={styles.input_ph + (props.value ? ` ${styles.active}` : "")}>
         {props.name}
       </p>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./App.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import "./App.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
