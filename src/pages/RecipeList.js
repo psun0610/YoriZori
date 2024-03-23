9c037579faef6eccf5264120375cac107d8b4876
SunYoung <97274144+psun0610@users.noreply.github.com>
2024-03-24 00:32:22 +0900

    Fix: 네비게이션이 컨텐츠를 가리는 오류, 컨텐츠의 양이 적어도 화면 하단에 위치하도록 수정함

--- src/pages/RecipeList.js
+++ src/pages/RecipeList.js
@@ -6,40 +6,46 @@ import Recipe from "../components/Recipe";
 const RecipeList = () => {
   return (
     <div>
-      <div className={styles.up}>
-        <div className={styles.list}>
-          <div className={styles.uplist}>
-            <p>전체</p>
-            <p>메인요리</p>
-            <p>밑반찬</p>
-            <p>면/만두</p>
-            <p>국/찌개</p>
-            <p>간식</p>
+      <div id="wrapper">
+        <div className={styles.up}>
+          <div className={styles.list}>
+            <div className={styles.uplist}>
+              <p>전체</p>
+              <p>메인요리</p>
+              <p>밑반찬</p>
+              <p>면/만두</p>
+              <p>국/찌개</p>
+              <p>간식</p>
+            </div>
+            <div className={styles.downlist}>
+              <p>샐러드</p>
+              <p>해장</p>
+              <p>밥/죽/떡</p>
+              <p>야식</p>
+              <p>디저트</p>
+            </div>
           </div>
-          <div className={styles.downlist}>
-            <p>샐러드</p>
-            <p>해장</p>
-            <p>밥/죽/떡</p>
-            <p>야식</p>
-            <p>디저트</p>
+          <div className={styles.search}>
+            <input
+              className={styles.search_input}
+              type="text"
+              placeholder="재료 검색하기"
+            ></input>
+            <img
+              className={styles.search_img}
+              src="../images/search.png"
+              alt="search"
+            ></img>
           </div>
         </div>
-        <div className={styles.search}>
-          <input
-            className={styles.search_input}
-            type="text"
-            placeholder="재료 검색하기"
-          ></input>
-          <img className={styles.search_img} src="../images/search.png" alt="search"></img>
+        <div className={styles.recipe_box}>
+          <Recipe />
+          <Recipe />
+          <Recipe />
+          <Recipe />
+          <Recipe />
+          <Recipe />
         </div>
-      </div>
-      <div className={styles.recipe_box}>
-      <Recipe/>
-      <Recipe/>
-      <Recipe/>
-      <Recipe/>
-      <Recipe/>
-      <Recipe/>
       </div>
       <Navigation />
     </div>
--- src/pages/ShoppingBasket.js
+++ src/pages/ShoppingBasket.js
@@ -5,36 +5,37 @@ import Navigation from "../components/Navigation";
 const ShoppingBasket = () => {
   return (
     <div>
-      <header className={styles.basket_header}>ShoppingBasket</header>
+      <div id="wrapper">
+        <header className={styles.basket_header}>ShoppingBasket</header>
 
-      <div className={styles.shopping_basket_container}>
-        <div className={styles.basket_info}>
-          <span className={styles.ingredient_count}>담은 재료 개</span>
-          <button className={styles.select_button}>선택하기</button>
-        </div>
+        <div className={styles.shopping_basket_container}>
+          <div className={styles.basket_info}>
+            <span className={styles.ingredient_count}>담은 재료 개</span>
+            <button className={styles.select_button}>선택하기</button>
+          </div>
 
-        <div className={styles.basket_items}>
-          <div className={styles.basket_item}>
-            <img
-              src="https://image.zdnet.co.kr/2019/01/15/sini_1zwN8DfI5uThk94.jpg"
-              className={styles.item_image}
-            />
-            <span className={styles.item_name}>Item 1</span>
-            <div className={styles.item_button}>
-              <button className={styles.item_cancel}>
-                <img className={styles.pin} src="/images/pin1.png" />
-              </button>
-              <button className={styles.item_cancel}>
-                <img className={styles.cancel} src="/images/cancel.png" />
-              </button>
+          <div className={styles.basket_items}>
+            <div className={styles.basket_item}>
+              <img
+                src="https://image.zdnet.co.kr/2019/01/15/sini_1zwN8DfI5uThk94.jpg"
+                className={styles.item_image}
+              />
+              <span className={styles.item_name}>Item 1</span>
+              <div className={styles.item_button}>
+                <button className={styles.item_cancel}>
+                  <img className={styles.pin} src="/images/pin1.png" />
+                </button>
+                <button className={styles.item_cancel}>
+                  <img className={styles.cancel} src="/images/cancel.png" />
+                </button>
+              </div>
             </div>
           </div>
-          
-        </div>
 
-        <button className={styles.add_button}>
-          재료추가<img className={styles.add} src="/images/add.png"></img>
-        </button>
+          <button className={styles.add_button}>
+            재료추가<img className={styles.add} src="/images/add.png"></img>
+          </button>
+        </div>
       </div>
       <Navigation />
     </div>
