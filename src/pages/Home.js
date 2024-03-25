2f2f1cbf4f84bc231f87eecc589c0b96e6fe8b6c
SunYoung <psy010610@naver.com>
2024-03-25 14:26:35 +0900

    Fix: 세로 가운데 정렬 필요한 페이지 flex 적용

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,17 +5,17 @@ import Button from "../components/Button";
 
 const Home = () => {
   return (
-    <div>
-      <div className={styles.home_img_container}>
-        <img src="../../images/logo.jpg"></img>
-      </div>
+    <div className={styles.home}>
+      <img src="../../images/logo.jpg" className={styles.logo}></img>
       <p className={styles.home_first}>처음 오셨나요?</p>
-      <Link to="/join">
-        <Button name="네, 처음이에요 !"></Button>
-      </Link>
-      <Link to="/login">
-        <Button name="아뇨, 회원이에요 !" color="white"></Button>
-      </Link>
+      <div className={styles.home_buttons}>
+        <Link to="/join">
+          <Button name="네, 처음이에요 !"></Button>
+        </Link>
+        <Link to="/login">
+          <Button name="아뇨, 회원이에요 !" color="white"></Button>
+        </Link>
+      </div>
       <footer></footer>
     </div>
   );
