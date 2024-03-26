f642473a0dd2994ad1f39869a3e3ef8d6523eedd
zzihoos <crient0204@naver.com>
2024-03-26 12:28:36 +0900

    feat: 장바구니 페이지 삭제, 고정, 고정해제, 장바구니 재료 카운터 기능 추가

---
+++ src/components/Shoppinglist.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "../styles/shoppingbasket.module.css";
+
+const Shoppinglist = () => {
+  return (
+    
+     
+        <div className={styles.basket_item}>
+          <img
+            src="https://image.zdnet.co.kr/2019/01/15/sini_1zwN8DfI5uThk94.jpg"
+            className={styles.item_image}
+          />
+          <span className={styles.item_name}>Item 1</span>
+          <div className={styles.item_button}>
+            <button className={styles.item_cancel}>
+              <img className={styles.pin} src="/images/pin1.png" />
+            </button>
+            <button className={styles.item_cancel}>
+              <img className={styles.cancel} src="/images/cancel.png" />
+            </button>
+          </div>
+        </div>
+      
+    
+  );
+};
+
+export default Shoppinglist;
--- src/pages/Recipe.js
+++ src/pages/Recipe.js
@@ -1,7 +1,50 @@
-import React from "react";
+// TodoList.js
+
+import React, { useState } from 'react';
 
 const Recipe = () => {
-  return <div>Recipe</div>;
+  const [todos, setTodos] = useState([]);
+  const [inputText, setInputText] = useState('');
+
+  const handleInputChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (inputText.trim() !== '') {
+      setTodos([...todos, { text: inputText, pinned: false }]);
+      setInputText('');
+    }
+  };
+
+  const handleTogglePin = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos[index].pinned = !updatedTodos[index].pinned;
+    if (updatedTodos[index].pinned) {
+      const pinnedTodo = updatedTodos.splice(index, 1);
+      updatedTodos.unshift(pinnedTodo[0]);
+    } else {
+      const unpinnedTodo = updatedTodos.splice(index, 1);
+      updatedTodos.push(unpinnedTodo[0]);
+    }
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <input type="text" value={inputText} onChange={handleInputChange} />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index} style={{ textDecoration: todo.pinned ? 'underline' : 'none' }}>
+            <span>{todo.text}</span>
+            <button onClick={() => handleTogglePin(index)}>{todo.pinned ? 'Unpin' : 'Pin'}</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Recipe;
