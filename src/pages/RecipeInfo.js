673ec6288be2acd49b674f5141fc097ba2203547
SunYoung <psy010610@naver.com>
2024-03-31 00:58:01 +0900

    Feat: RecipeList, RecipeInfo 페이지 css 반응형으로 수정

--- src/pages/RecipeInfo.js
+++ src/pages/RecipeInfo.js
@@ -1,41 +1,72 @@
 import React from "react";
-import styles from "../styles/recipeinfo.module.css";
+import styles from "../styles/RecipeInfo.module.css";
+import Navigation from "../components/Navigation";
 
 const RecipeInfo = () => {
   return (
     <div>
       <div id="wrapper">
-        <div>
-          <img
-            className={styles.recipe_img}
-            src="./images/mainpage_food_image.jpg"
-          />
+        <div className={styles.recipe_img}>
+          <img src="./images/mainpage_food_image.jpg" />
         </div>
-        <div className={styles.info_up}>
-          <div className={styles.info_left}>
-            <span className={styles.title}>계란찜</span>
-            <span>가진 재료 |</span>
-            <span>없는 재료 |</span>
-          </div>
 
-          <div className={styles.button}>
-            <button className={styles.basket_button}>
-              <img className={styles.basket_img} src="./images/basket_button.png" />
-            </button>
-            <button className={styles.bookmark_button}>
-              <img className={styles.bookmark} src="./images/bookmark.png" />
-            </button>
+        <div className={styles.container}>
+          <div className={styles.info_up}>
+            <div>
+              <div className={styles.info_between}>
+                <h1 className={styles.title}>계란찜</h1>
+                <div className={styles.button_container}>
+                  <button className={styles.basket_button}>
+                    <p>장바구니에 추가</p>
+                    <svg
+                      width="14"
+                      height="14"
+                      viewBox="0 0 14 14"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <g clipPath="url(#clip0_219_17)">
+                        <path
+                          fillRule="evenodd"
+                          clipRule="evenodd"
+                          d="M9.1875 3.0625C9.1875 2.48234 8.95703 1.92594 8.5468 1.5157C8.13656 1.10547 7.58016 0.875 7 0.875C6.41984 0.875 5.86344 1.10547 5.4532 1.5157C5.04297 1.92594 4.8125 2.48234 4.8125 3.0625V3.5H9.1875V3.0625ZM10.0625 3.0625V3.5H13.125V12.25C13.125 12.7141 12.9406 13.1592 12.6124 13.4874C12.2842 13.8156 11.8391 14 11.375 14H2.625C2.16087 14 1.71575 13.8156 1.38756 13.4874C1.05937 13.1592 0.875 12.7141 0.875 12.25V3.5H3.9375V3.0625C3.9375 2.25027 4.26016 1.47132 4.83449 0.896985C5.40882 0.322655 6.18777 0 7 0C7.81223 0 8.59118 0.322655 9.16551 0.896985C9.73984 1.47132 10.0625 2.25027 10.0625 3.0625ZM7.4375 7C7.4375 6.88397 7.39141 6.77269 7.30936 6.69064C7.22731 6.60859 7.11603 6.5625 7 6.5625C6.88397 6.5625 6.77269 6.60859 6.69064 6.69064C6.60859 6.77269 6.5625 6.88397 6.5625 7V8.3125H5.25C5.13397 8.3125 5.02269 8.35859 4.94064 8.44064C4.85859 8.52269 4.8125 8.63397 4.8125 8.75C4.8125 8.86603 4.85859 8.97731 4.94064 9.05936C5.02269 9.14141 5.13397 9.1875 5.25 9.1875H6.5625V10.5C6.5625 10.616 6.60859 10.7273 6.69064 10.8094C6.77269 10.8914 6.88397 10.9375 7 10.9375C7.11603 10.9375 7.22731 10.8914 7.30936 10.8094C7.39141 10.7273 7.4375 10.616 7.4375 10.5V9.1875H8.75C8.86603 9.1875 8.97731 9.14141 9.05936 9.05936C9.14141 8.97731 9.1875 8.86603 9.1875 8.75C9.1875 8.63397 9.14141 8.52269 9.05936 8.44064C8.97731 8.35859 8.86603 8.3125 8.75 8.3125H7.4375V7Z"
+                          fill="white"
+                        />
+                      </g>
+                      <defs>
+                        <clipPath id="clip0_219_17">
+                          <rect width="14" height="14" fill="white" />
+                        </clipPath>
+                      </defs>
+                    </svg>
+                  </button>
+                  <button className={styles.bookmark}>
+                    <img src="./images/bookmark.png" />
+                    <p>6</p>
+                  </button>
+                </div>
+              </div>
+              <div className={styles.title_p}>
+                <p>
+                  가진 재료<p>|</p>
+                </p>
+                <p>
+                  없는 재료<p>|</p>
+                </p>
+              </div>
+            </div>
           </div>
+          <div className={styles.cook}>
+            <img
+              className={styles.cook_img}
+              src="./images/mainpage_food_image.jpg"
+            />
+            <span>1.계란을 풀어준다</span>
+          </div>
+          <div />
         </div>
-        <div className={styles.cook}>
-          <img
-            className={styles.cook_img}
-            src="./images/mainpage_food_image.jpg"
-          />
-          <span>1.계란을 풀어준다</span>
-        </div>
-        <div />
       </div>
+      <Navigation />
     </div>
   );
 };
