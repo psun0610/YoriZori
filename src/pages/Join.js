a919b56e82b4d1a92a855576d30e41ee11c88b63
SunYoung <psy010610@naver.com>
2024-03-30 21:05:53 +0900

    Feat: 회원가입 기능 추가 (아이디 중복, 빈 항목, 비밀번호 일치 체크 추가)

--- src/pages/Join.js
+++ src/pages/Join.js
@@ -1,45 +1,144 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "../styles/LoginJoin.module.css";
-import LoginSignupInput from "../components/LoginJoinInput";
+import axios from "axios";
 import "../App.css";
 
 const Join = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [passwordMatchError, setPasswordMatchError] = useState(false);
+  const userURL = "http://localhost:8080/users";
+  const navigate = useNavigate();
+  const [user, setUser] = useState({
+    userName: "",
+    password: "",
+    confirmPassword: "",
+    nickName: "",
+  });
 
-  const handleConfirmPasswordChange = e => {
-    const { value } = e.target;
-    setConfirmPassword(value);
-    // 비밀번호 확인이 변경될 때마다 비밀번호와 일치 여부를 확인하여 상태 업데이트
-    setPasswordMatchError(value !== password);
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
+
+  // Validation 설정
+  const [valid, setValid] = useState({
+    validEmpty: false,
+    passwordNoMatch: false,
+    idDuplicate: false,
+  });
+
+  // 비밀번호 Match Validation
+  useEffect(() => {
+    setValid({
+      ...valid,
+      passwordNoMatch: user.password !== user.confirmPassword,
+    });
+  }, [user.password, user.confirmPassword]);
+
+  // 모든 항목을 채워달라는 에러 메시지 없애는 useEffect
+  useEffect(() => {
+    setValid(prevValid => ({
+      ...prevValid,
+      validEmpty: false,
+    }));
+  }, [user.userName, user.password, user.confirmPassword, user.nickName]);
+
+  const handleSubmitClick = e => {
+    e.preventDefault();
+    // input이 비어있는지 확인
+    if (
+      user.userName.trim() === "" ||
+      user.password.trim() === "" ||
+      user.confirmPassword.trim() === "" ||
+      user.nickName.trim() === ""
+    ) {
+      setValid({ ...valid, validEmpty: true });
+      return;
+    } else {
+      setValid({ ...valid, validEmpty: false });
+    }
+
+    // 회원가입
+    axios
+      .post(userURL, {
+        name: user.userName,
+        password: user.password,
+        nickname: user.nickName,
+      })
+      .then(() => {
+        navigate("/avoidance");
+      })
+      .catch(error => {
+        if (error.response.data.message === "이미 존재하는 아이디입니다.")
+          setValid({ ...valid, idDuplicate: true });
+        return;
+      });
   };
 
   return (
     <form className={styles.home}>
       <h1 className={styles.join_h1}>회원가입</h1>
-      <LoginSignupInput
-        name="아이디"
-        value={username}
-        onChange={e => setUsername(e.target.value)}
-      />
-      <LoginSignupInput
-        name="비밀번호"
-        type="password"
-        value={password}
-        onChange={e => setPassword(e.target.value)}
-      />
-      <LoginSignupInput
-        name="비밀번호 확인"
-        type="password"
-        value={confirmPassword}
-        onChange={handleConfirmPasswordChange}
-      />
-      {passwordMatchError && (
-        <p className={styles.inconsistency}>비밀번호가 일치하지 않습니다.</p>
+
+      <div className={styles.input_container}>
+        <input
+          name="userName"
+          value={user.userName}
+          onChange={handleChange}
+        ></input>
+        <p className={user.userName ? ` ${styles.active}` : ""}>아이디</p>
+      </div>
+      {valid.idDuplicate && (
+        <p className={styles.inconsistency}>이미 존재하는 아이디입니다</p>
       )}
-      <input type="submit" value="확인" className={`button pink_back`}></input>
+
+      <div className={styles.input_container}>
+        <input
+          name="password"
+          value={user.password}
+          onChange={handleChange}
+          type="password"
+        ></input>
+        <p className={user.password ? ` ${styles.active}` : ""}>비밀번호</p>
+      </div>
+
+      <div className={styles.input_container}>
+        <input
+          name="confirmPassword"
+          value={user.confirmPassword}
+          onChange={handleChange}
+          type="password"
+        ></input>
+        <p className={user.confirmPassword ? ` ${styles.active}` : ""}>
+          비밀번호 확인
+        </p>
+      </div>
+
+      {valid.passwordNoMatch && (
+        <p className={styles.inconsistency}>비밀번호가 일치하지 않습니다</p>
+      )}
+
+      <div className={styles.input_container}>
+        <input
+          name="nickName"
+          value={user.nickName}
+          onChange={handleChange}
+        ></input>
+        <p className={user.nickName ? ` ${styles.active}` : ""}>닉네임</p>
+      </div>
+
+      {valid.validEmpty && (
+        <p className={styles.inconsistency}>모든 항목을 채워주세요</p>
+      )}
+
+      <input
+        type="submit"
+        value="확인"
+        className={`button pink_back`}
+        style={{ marginTop: "20px" }}
+        onClick={handleSubmitClick}
+      ></input>
     </form>
   );
 };
