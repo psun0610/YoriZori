d86a3f6d6a57f89027d67984d0793fc6430840b5
SunYoung <psy010610@naver.com>
2024-05-20 15:18:06 +0900

    Deploy: 배포된 API로 주소 변경

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,7 +6,8 @@ import styles from "../styles/LoginJoin.module.css";
 import "../App.css";
 
 const Login = () => {
-  const baseURL = "http://localhost:8080";
+  const baseURL =
+    "http://ec2-13-125-66-179.ap-northeast-2.compute.amazonaws.com:8080";
   const navigate = useNavigate();
   const [userName, setUsername] = useState("");
   const [password, setPassword] = useState("");
