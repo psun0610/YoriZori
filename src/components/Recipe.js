8295182ba6fef699c2d841244dd6a692edf568a2
SunYoung <97274144+psun0610@users.noreply.github.com>
2024-05-20 20:00:00 +0900

    Feat: 메인페이지 오늘의 추천 레시피 기능 추가

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -6,7 +6,7 @@ import AxiosAuth from "../components/AxiosAuth";
 
 function Recipe(props) {
   const [bookmarkCheck, setBookmarkCheck] = useState({});
-  const baseURL = "http://localhost:8080";
+  const baseURL = process.env.REACT_APP_BASE_URL;
   const { searchText, category } = props;
   const { id } = useParams();
   const [recipes, setRecipes] = useState([]);
@@ -30,7 +30,7 @@ function Recipe(props) {
     }
   };
 
-  const fetchBookmarkCheck = async (recipeId) => {
+  const fetchBookmarkCheck = async recipeId => {
     try {
       const response = await AxiosAuth.get(`/users/check-bookmark`, {
         params: { recipeId },
@@ -40,7 +40,10 @@ function Recipe(props) {
         [recipeId]: response.data.bookmarkCheck,
       }));
     } catch (error) {
-      console.error(`Error fetching bookmark status for recipe ${recipeId}:`, error);
+      console.error(
+        `Error fetching bookmark status for recipe ${recipeId}:`,
+        error,
+      );
     }
   };
 
