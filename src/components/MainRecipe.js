f6fea5ec8f5d11905ae3164d3234e337d6855b85
SunYoung <97274144+psun0610@users.noreply.github.com>
2024-05-21 19:21:45 +0900

    Fix: 메인페이지 추천 레시피 AuthUser, Guest로 분기, 냉장고 재료 이미지 src 변경

--- src/components/MainRecipe.js
+++ src/components/MainRecipe.js
@@ -1,28 +1,29 @@
 import React from "react";
 import styles from "../styles/Main.module.css";
 
-function MainRecipe({ recipe }) {
+function MainRecipe({ recipe, authUser }) {
   return (
     <>
-      <img src={recipe.imageUrl} alt={recipe.title} />
+      <img src={recipe.imageUrl} alt={recipe.name} />
       <div>
         <div className={styles.recipe_title}>
-          <h2>{recipe.title}</h2>
+          <h2>{recipe.name}</h2>
         </div>
-        {recipe.insufficientIngredientsCount ? (
-          <div>
-            <p className={styles.lack_descript}>
-              부족한 재료 <span>{recipe.insufficientIngredientsCount}</span>개
-            </p>
-            <div className={styles.lack_ingredients}>
-              {recipe.insufficientIngredients.map((ingredient, i) => (
-                <div key={i}>{ingredient.name}</div>
-              ))}
+        {authUser &&
+          (recipe.insufficientIngredientsCount ? (
+            <div>
+              <p className={styles.lack_descript}>
+                부족한 재료 <span>{recipe.insufficientIngredientsCount}</span>개
+              </p>
+              <div className={styles.lack_ingredients}>
+                {recipe.insufficientIngredients.map((ingredient, i) => (
+                  <div key={i}>{ingredient.name}</div>
+                ))}
+              </div>
             </div>
-          </div>
-        ) : (
-          <p className={styles.success_message}>지금 만들 수 있어요!</p>
-        )}
+          ) : (
+            <p className={styles.success_message}>지금 만들 수 있어요!</p>
+          ))}
       </div>
     </>
   );
