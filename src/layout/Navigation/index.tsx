1a4610f5b486359fc6be897173cc92513e4c2c04
SunYoung <97274144+psun0610@users.noreply.github.com>
2025-01-24 22:11:43 +0900

    HotFix: Invalid Hook Call Warning 수정

--- src/layout/Navigation/index.tsx
+++ src/layout/Navigation/index.tsx
@@ -3,10 +3,9 @@ import * as S from "./Navigation.style";
 import { MenuDatas } from "./MenuDatas";
 import NavButton from "./NavButton";
 
-const currentURL = useLocation().pathname;
-
 // 네비게이션 사용시, 네비게이션 앞의 모든 내용을 #wrapper로 묶어야 함! (특히 컨텐츠가 적은 페이지)
 const Navigation = () => {
+  const currentURL = useLocation().pathname;
   return (
     <S.NavContainer>
       {MenuDatas.map((menu, index) => (
